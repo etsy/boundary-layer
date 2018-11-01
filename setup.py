6e99b06468132fdd11aef487132918fbf5f74269
Kevin McHale <mchalek@gmail.com>
2018-11-01 17:14:48 -0400

    add keywords

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setuptools.setup(
         'Topic :: Utilities',
     ],
     license = 'Apache License 2.0',
+    keywords = 'airflow',
     zip_safe = False,
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
 )
