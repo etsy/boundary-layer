7c259beca283ce9789516f64a441cf4d6579feeb
Kevin McHale <mchalek@gmail.com>
2018-11-07 10:16:25 -0500

    Revert "fix run_coveralls script to not run on pull requests"
    This reverts commit 3ce8bd148f589c9995eb812507bdf62dd521f746.
    
    Because coveralls running on pull requests turns out to be awesome.

--- run_coveralls.sh
+++ run_coveralls.sh
@@ -10,11 +10,6 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
     exit 0
 fi
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "Not running coveralls because this build is for a pull request"
-    exit 0
-fi
-
 if [ "$TOXENV" != "py27" ]; then
     echo "Not running coveralls because we are in the build environment '$TOX_ENV_NAME'"
     exit 0
