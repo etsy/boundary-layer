3f686a7b102fd00b607843e914777eaf1eeb7999
Kevin McHale <mchalek@gmail.com>
2020-07-20 12:55:33 -0400

    streamline a little

--- release.py
+++ release.py
@@ -18,6 +18,7 @@ This will:
 Once the tag is created remotely, a github actions workflow should run the publish build, and release the new version of boundary-layer to pypi.
 """
 import argparse
+from collections import namedtuple
 import re
 import shlex
 import subprocess
@@ -97,12 +98,20 @@ def git_checkout(remote_name, branch_name):
     subprocess.check_call(shlex.split(f'git checkout {ref}'))
 
 
-def check_git_state():
+def get_git_state():
     output = subprocess.check_output(
-        shlex.split('git status --porcelain'),
+        shlex.split('git status -b --porcelain'),
         encoding='utf-8'
-    ).strip()
-    if output:
+    ).strip().split('\n')
+
+    return namedtuple('GitState', ['branch', 'files'])(
+        branch=output_lines[0],
+        files=output_lines[1:],
+    )
+
+
+def check_git_state(state):
+    if state.files:
         raise Exception(
             f'Cannot release: unclean git state:\n{output}'
         )
@@ -129,18 +138,18 @@ def create_and_push_tag(remote_name, tag_name):
     print('Tag pushed successfully.')
 
 
-def get_current_branch():
-    output = subprocess.check_output(
-        shlex.split('git status -b --porcelain'), encoding='utf-8'
-    ).strip().split('\n')[0]
-
-    # at this point, output has the form
+def parse_current_branch(state):
+    # state.branch has the form
     # `## <branch-name>...<upstream> [<ahead|behind> <number-of-commits>]`
 
     pattern = r'## (?P<branch_name>[a-zA-Z0-9_/-]+)(\.\.\.(?P<remote>[a-z0-9A-Z_/-]+)( \[(?P<ahead_behind>(ahead|behind) \d+)\])?)?'
-    m = re.match(pattern, output)
+    m = re.match(pattern, state.branch).groupdict()
 
-    return m.groupdict()
+    return namedtuple('GitBranch', ['name', 'remote', 'ahead_behind'])(
+        m.get('branch_name'),
+        m.get('remote'),
+        m.get('ahead_behind')
+    )
 
 
 def get_version(bump_type, force_version):
@@ -162,23 +171,22 @@ def do_release(*, remote_name, branch_name, bump_type, force_version):
     check_remote(remote_name)
     fetch_latest(remote_name, branch_name)
 
-    current_branch = get_current_branch()
-    if current_branch.get('remote') == f'{remote_name}/{branch_name}' and current_branch.get('ahead_behind'):
+    current_branch = get_current_branch(get_git_state())
+    if current_branch.remote == f'{remote_name}/{branch_name}' and current_branch.ahead_behind:
         raise Exception(
-            f'Local changes found on branch `{current_branch.get("branch_name")}` which tracks `{remote_name}/{branch_name}`!  This is probably unintended.  Please reconcile your local state before proceeding.'
+            f'Local changes found on branch `{current_branch.name}` which tracks `{remote_name}/{branch_name}`!  This is probably unintended.  Please reconcile your local state before proceeding.'
         )
 
     try:
         git_checkout(remote_name, branch_name)
-        check_git_state()
+        check_git_state(get_git_state())
         create_and_push_tag(
             remote_name,
             tag_name=get_version(bump_type, force_version),
         )
     finally:
-        original_branch = current_branch.get('branch_name')
-        if original_branch:
-            git_checkout(None, original_branch)
+        if current_branch.name:
+            git_checkout(None, current_branch.name)
 
 
 if __name__ == '__main__':
